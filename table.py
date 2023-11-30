70f95dad4e7578f65d488810c25a373419ab18f9
Tran Quoc Hieu <ngoquochieu1211@gmail.com>
2023-11-30 08:33:49 +0700

    format data input

--- table.py
+++ table.py
@@ -5,6 +5,19 @@ from scipy.stats import binom
 r_values = list(range(11))
 dist = [binom.pmf(r, 10, 0.5) for r in r_values]
 
+default_unit_data = [
+        ["unit_1", 2],
+        ["unit_2", 2],
+        ["unit_3", 2],
+        ["unit_4", 2],
+        ["unit_5", 2],
+        ["unit_6", 2],
+        ["unit_7", 2],
+        ["unit_8", 2]
+    ]
+
+default_product_data = [["product_1", 2], ["product_2", 2],["product_3", 2], ["product_4", 2],["product_5", 2]]
+
 supply_data_frame = []
 
 try:
@@ -73,8 +86,8 @@ try:
         product_price_data.append(["product_" + str(i), int(line[1])])
 except OSError: # default value
     print("Can not read product.txt file")
-    product_cost_data = [["product_1", 2], ["product_2", 2],["product_3", 2], ["product_4", 2],["product_5", 2]]
-    product_price_data = [["product_1", 1], ["product_2", 1],["product_3", 1], ["product_4", 1],["product_5", 1]]
+    product_cost_data = default_product_data
+    product_price_data = default_product_data
 
 product_cost = pd.DataFrame(
     product_cost_data,
@@ -87,49 +100,25 @@ product_selling_price = pd.DataFrame(
 ).set_index("product")
 
 unit_price_data = []
+preorder_cost_unit_data = []
 
 try:
     lines = open("unit.txt").readlines()
     for j in range (1,9):
         unit_price_data.append(["unit_"+str(j), int(lines[j].split()[0])])
+        preorder_cost_unit_data.append(["unit_"+str(j), int(lines[j].split()[1])])
 except OSError: # default value
     print("Can not read unit.txt file")
-    unit_price_data = [
-        ["unit_1", 2],
-        ["unit_2", 2],
-        ["unit_3", 2],
-        ["unit_4", 2],
-        ["unit_5", 2],
-        ["unit_6", 2],
-        ["unit_7", 2],
-        ["unit_8", 2]
-    ],
+    unit_price_data = default_unit_data
+    preorder_cost_unit_data = default_unit_data
 
 unit_selling_price = pd.DataFrame(
     unit_price_data,
     columns=["unit", "selling price\n(s_j)"]
 ).set_index("unit")
 
-preorder_cost_data = []
-try:
-    lines = open("preorder_cost_unit.txt").readlines()
-    for i in range(1,9):
-        preorder_cost_data.append(["unit_" + str(i), int(lines[i].split()[0])])
-except OSError:
-    print("Can not read preorder_cost_unit.txt")
-    preorder_cost_data = [
-        ["unit_1", 2],
-        ["unit_2", 2],
-        ["unit_3", 2],
-        ["unit_4", 2],
-        ["unit_5", 2],
-        ["unit_6", 2],
-        ["unit_7", 2],
-        ["unit_8", 2]
-    ],
-
 preorder_cost = pd.DataFrame(
-    preorder_cost_data,
+    preorder_cost_unit_data,
     columns=["unit", "preorder cost\n(b_j)"]
 ).set_index("unit")
 
